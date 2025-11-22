7645660b3906fa933491321c2fb2e168a3e61024
wovesiek <icecap.curvy_2m@icloud.com>
2025-11-22 17:05:29 +0100

    Add files via upload

---
+++ start-mining-equitypay.bat
@@ -0,0 +1,9 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --algorithm yespowereqpay --pool yespowerEQPAY.eu.mine.zpool.ca:6249 --wallet eqpay-wallet
+pause
